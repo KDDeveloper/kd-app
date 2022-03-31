64e5e13a24381fddf88078bd903077d5ce79c684
KDDeveloper <kddfreelance@example.com>
2022-03-31 19:49:06 +0530

    updates

--- src/pages/projectsPage/projectsPage.jsx
+++ src/pages/projectsPage/projectsPage.jsx
@@ -6,21 +6,25 @@ const ProjectsPage = () =>{
     const projects = [{
         name:"Nuva",
         img:`${require("../../resources/images/nuvaSS.png")}`,
-        link:"https://condescending-lamport-d51835.netlify.app/"
+        link:"https://condescending-lamport-d51835.netlify.app/",
+        github:"https://github.com/KDDeveloper/new-nuva-app"
     },{
         name:"CRM",
         img:`${require("../../resources/images/crmSS.png")}`,
-        link:"https://kd-crm.netlify.app/summary"
+        link:"https://kd-crm.netlify.app/summary",
+        github:"https://github.com/KDDeveloper/CRM-backend"
     },
     {
         name:"Todo App",
         img:`${require("../../resources/images/todoSS.png")}`,
-        link:"https://kd-todo-notes.netlify.app/login"
+        link:"https://kd-todo-notes.netlify.app/login",
+        github:"https://github.com/KDDeveloper/todo-app-backend"
     },
     {
         name:"Money manager",
         img:`${require("../../resources/images/moneyManagerSS.png")}`,
-        link:"https://kd-money-manager.netlify.app/home"    
+        link:"https://kd-money-manager.netlify.app/home",
+        github:"https://github.com/KDDeveloper/money-manager-backend"    
     }]
     return(
         <>
@@ -34,7 +38,7 @@ const ProjectsPage = () =>{
                                 <div className="project-name"><h3>{project.name}</h3></div>
                                 <div className="project-buttons">
                                     <div className="visit-btn"><a target="_blank" href={project.link}>Visit</a></div>
-                                    <div className="github-btn"><a href="http://"><GithubIcon/>Github</a></div>
+                                    <div className="github-btn"><a target="_blank" href={project.github}><GithubIcon/>Github</a></div>
                                 </div>
                             </div>
                         )
